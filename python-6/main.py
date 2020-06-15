bafac654ef8932b5f3a8508f28115a1dfd6c0f02
Carlos Eduardo Lourenco <engenharia.lourenco@gmail.com>
2020-06-15 10:54:33 -0300

    Desafio de programação orientada a objetos

---
+++ python-6/main.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+
+
+class Department:
+    def __init__(self, name, code):
+        self.name = name
+        self.code = code
+
+
+class Employee(ABC):
+    def __init__(self, code, name, salary, department):
+        self.code = code
+        self.name = name
+        self.salary = salary
+        self.__department = department
+
+    @abstractmethod
+    def calc_bonus(self):
+        pass
+
+    def get_hours(self):
+        return 8
+
+    def get_department(self):
+        return self.__department.name
+
+    def set_department(self, new_department):
+        self.__department.name = new_department
+
+
+class Manager(Employee):
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary, Department('managers', 1))
+
+    def calc_bonus(self):
+        return self.salary * 0.15
+
+
+class Seller(Employee):
+    def __init__(self, code, name, salary):
+        super().__init__(code, name, salary, Department('sellers', 2))
+        self.__sales = 0
+
+    def calc_bonus(self):
+        return self.__sales * 0.15
+
+    def get_sales(self):
+        return self.__sales
+
+    def put_sales(self, sales):
+        self.__sales += sales
